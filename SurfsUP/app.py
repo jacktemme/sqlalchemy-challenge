ac04cbd175a41899db26fd730e7b8b9cd3d1a107
Jack Temme <jtemme7@gmail.com>
2024-04-25 16:08:39 -0600

    updates

--- SurfsUP/app.py
+++ SurfsUP/app.py
@@ -1,22 +1,30 @@
 # Import the dependencies.
-
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
 
-
 # reflect an existing database into a new model
-
+engine = create_engine("sqlite:////Users/jacktemme/Desktop/Data_Challenges/sqlalchemy-challenge/Resources/hawaii.sqlite")
 # reflect the tables
-
+Base = automap_base()
+Base.prepare(autoload_with = engine)
 
 # Save references to each table
-
+station = Base.classes.station
+measurement = Base.classes.measurement
 
 # Create our session (link) from Python to the DB
-
+session = Session(bind = engine)
 
 #################################################
 # Flask Setup
@@ -25,6 +33,9 @@
 
 
 
+
 #################################################
 # Flask Routes
 #################################################
+
+
