d0bd5724b305e3e1df7d8d8a7973b7bdcdd19dd7
Jack Temme <jtemme7@gmail.com>
2024-04-27 11:46:47 -0600

    finished up app.py

--- SurfsUP/app.py
+++ SurfsUP/app.py
@@ -1,5 +1,5 @@
 # Import the dependencies.
-import datetime as dt
+
 import numpy as np
 import pandas as pd
 import sqlalchemy
@@ -7,7 +7,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
-
+from datetime import datetime as dt
 
 #################################################
 # Database Setup
@@ -42,17 +42,18 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return (
-        "Welcome to an API on Hawaii Weather Data <br/>"
+        "Welcome to an API on Hawaii Weather Data <br/><br/>"
         "The available routes are: <br/>"
         "/api/v1.0/precipitation <br/>"
         "/api/v1.0/stations <br/>"
         "/api/v1.0/tobs <br/>"
-        "/api/v1.0/2016-08-23 and /api/v1.0/2012-02-26/2017-01_24 <br/>"
+        "/api/v1.0/start (input start date as YYYY-MM-DD) <br/>"
+        "/api/v1.0/start/end (input start and end date AS YYYY-MM-DD ) <br/>"
         "The oldest date of this database is: 2010-01-01 <br/>"
         "The newest date of this database is: 2017-08-23 "
  )
 
-# Route for date and precipitation in the last 12 months
+# Route for date and precipitation in the last year of data
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -66,59 +67,68 @@ def precipitation():
 
 
 # Route for list of stations
-
 @app.route("/api/v1.0/stations")
 def stations():
 
     station_list = [s.station for s in (session.query(station.station).all())]
     return jsonify(station_list)
 
-# Route for temperature data from the most active station 'USC00519281'
-# in the previous year
-
+# Route for temperature data from the most active station 'USC00519281' in the previous year
 @app.route("/api/v1.0/tobs")
 def tobs():
 
     active_station_temp = session.query(measurement.tobs, measurement.date)\
                         .filter(measurement.station == 'USC00519281')\
                         .filter(measurement.date >= "2016-08-23").all()
-    
-    tobs_list = [(temp.tobs) for temp in active_station_temp]
+    tobs_dict ={}
+    for row in active_station_temp:
+        tobs_dict[row.date] = row.tobs
+    return jsonify(tobs_dict)
 
-    return jsonify(tobs_list)
-
-# Route for min average and max temperature for specified start range to end of dataset
-# Put a dictionary for added readability
-
+# Route for min, max and average temperature for specified start range to end of dataset
 @app.route("/api/v1.0/<start>")
 def start(start):
+    try:
+
+        # Check if start and end dates are in correct format (YYYY-MM-DD)
+        dt.strptime(start, '%Y-%m-%d')
+ 
+        sel = [func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)]
+        start_values = session.query(*sel).filter(measurement.date >= start).all()
+
+        # Put into a dictionary for added readability
+        start_dict = {"min": start_values[0][0], 
+                    "max" : start_values[0][1],
+                    "average": start_values[0][2]}
+        
+        return jsonify(start_dict)
     
-    sel = [func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)]
-    start_values = session.query(*sel).filter(measurement.date >= start).all()
+    except ValueError:
+         return jsonify({"error": "Character not found. Make sure its formatted YYYY-MM-DD"}), 404
 
-    start_dict = {"min": start_values[0][0], 
-                "max" : start_values[0][1],
-                "average": start_values[0][2]}
-    
-    return jsonify(start_dict)
-
-
-# Route for min average and max temperature for specified start range to specified end range
-# Put a dictionary for added readability
-
+# Route for min, max and average temperature for specified start range to specified end range
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
 
-    sel = [func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)]
-    start_end_values = session.query(*sel).filter(measurement.date >= start)\
-                    .filter(measurement.date <= end).all()
+    try:
 
-    start_end_dict = {"min": start_end_values[0][0], 
-                "max" : start_end_values[0][1],
-                "average": start_end_values[0][2]}
+        # Check if start and end dates are in correct format (YYYY-MM-DD)
+        dt.strptime(start, '%Y-%m-%d')
+        dt.strptime(end, '%Y-%m-%d')
     
-    return jsonify(start_end_dict)
+        sel = [func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)]
+        start_end_values = session.query(*sel).filter(measurement.date >= start)\
+                        .filter(measurement.date <= end).all()
 
+        # Put into a dictionary for added readability
+        start_end_dict = {"min": start_end_values[0][0], 
+                    "max" : start_end_values[0][1],
+                    "average": start_end_values[0][2]}
+        
+        return jsonify(start_end_dict)
+    
+    except ValueError:
+        return jsonify({"error": "Character not found. Make sure its formatted YYYY-MM-DD"}), 404
   
 if __name__ == "__main__":
     app.run()
